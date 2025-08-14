3f94198c0bb37f49c0203dc7402241304e95a2ec
sah024 <sarah.tumelero.da.silveira@gmail.com>
2025-08-14 11:46:09 -0300

    Add Logo

--- trab_filmes/resources/views/layouts/navigation.blade.php
+++ trab_filmes/resources/views/layouts/navigation.blade.php
@@ -3,6 +3,9 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
+
+                <x-application-logo></x-application-logo>
+                <p style="height:100%; font-weight: bold; font-style:italic;justify-content: center; align-content:center; padding:0.5rem; font-size:20px; color: #ffc300">The Picture House</p>
                 @if (Auth()->user() && Auth()->user()->is_admin)
 
                 <div class="space-x-8 sm:-my-px sm:ms-10 sm:flex">
